902f11beff1ff6024de361d44decee25b35dc5fc
Roy Huang <beersheep0109@gmail.com>
2018-01-13 22:48:10 +0800

    add form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,6 +11,7 @@ class PostsNew extends Component {
           type='text'
           {...field.input}
         />
+        {field.meta.error}
       </div>
     )
   }
@@ -23,25 +24,40 @@ class PostsNew extends Component {
           name="title"
           component={this.renderField}
         />
-
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
         />
-
         <Field
           label="Post Content"
           name="content"
           component={this.renderField}
         />
-          
       </form>
     )
   }
 }
 
+function validate(values) { // values is an object of user inputs
+  const errors = {}
+
+  if (!values.title) {
+    errors.title = "Please enter a title"
+  }
+
+  if (!values.content) {
+    errors.content = "Please enter some contents"
+  }
+
+  // the validation of the field associated with the field name.
+  // the object key of errors has to be identical to the field name.
+
+  return errors
+}
+
 export default reduxForm({
+  validate, // same as validate: validate
   form: 'PostsNewForm' // name of the form, has to be uniq
 })(PostsNew)
 
