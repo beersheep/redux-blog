7a5474f6f3570dd66324ebacff9178de1daedd2f
Roy Huang <beersheep0109@gmail.com>
2018-01-14 22:35:28 +0800

    create createPost action and wire up with the component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { createPost } from '../actions/'
 
 class PostsNew extends Component {
-  onSubmit = (values) => {
-    console.log(values)
+  onSubmit = (value) => {
+    this.props.createPost(value)
   }
 
   renderField(field) {
     const { error, touched } = field.meta
+    const errorClass = (touched && error) ? 'has-error' : ''
 
     return (
-      <div className="form-group">
+      <div className={`form-group ${errorClass}`}>
         <label>{field.label}</label>
         <input 
           className="form-control"
           type='text'
           {...field.input}
         />
-        {touched && error}
+        <div className="help-block">
+          {touched && error}
+        </div>
       </div>
     )
   }
@@ -26,26 +32,29 @@ class PostsNew extends Component {
     const { handleSubmit } = this.props
 
     return (
-      <form onSubmit={handleSubmit(this.onSubmit)}>
-        <Field 
-          label="Title"
-          name="title"
-          component={this.renderField}
-          validate={requireValidate("Please provide a title")}
-        />
-        <Field
-          label="Categories"
-          name="categories"
-          component={this.renderField}
-        />
-        <Field
-          label="Post Content"
-          name="content"
-          component={this.renderField}
-          validate={requireValidate("Please provide some contents")}
-        />
-        <button type="submit" className="btn btn-primary">Post</button>
-      </form>
+      <div>
+        <form onSubmit={handleSubmit(this.onSubmit)}>
+          <Field 
+            label="Title"
+            name="title"
+            component={this.renderField}
+            validate={requireValidate("Please provide a title")}
+          />
+          <Field
+            label="Categories"
+            name="categories"
+            component={this.renderField}
+          />
+          <Field
+            label="Post Content"
+            name="content"
+            component={this.renderField}
+            validate={requireValidate("Please provide some contents")}
+          />
+          <button type="submit" className="btn btn-primary">Post</button>
+          <Link to="/" className="btn btn-danger">Cancel</Link>
+        </form>
+      </div>
     )
   }
 }
@@ -54,25 +63,10 @@ const requireValidate = (message) => (value) => {
   return value ? '' : message
 }
 
-function validate(values) { // values is an object of user inputs
-  const errors = {}
-
-  if (!values.title) {
-    errors.title = "Please enter a title"
-  }
-
-  if (!values.content) {
-    errors.content = "Please enter some contents"
-  }
-
-  // the validation of the field associated with the field name.
-  // the object key of errors has to be identical to the field name.
-
-  return errors
-}
-
 export default reduxForm({
   // validate, // same as validate: validate
   form: 'PostsNewForm' // name of the form, has to be uniq
-})(PostsNew)
+})(
+  connect(null, { createPost })(PostsNew)
+)
 
